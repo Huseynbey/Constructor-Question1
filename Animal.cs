9f34b4ebd85cd4f60c6dc5c2197565dbd8828785
Huseyn Baymammadli <huseynnb@code.edu.az>
2023-01-25 17:36:34 +0400

    Constructor Question1

---
+++ Animal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    public class Animal
+    {
+        public string name;
+        public string breed;
+        public string color;
+        public int age;
+
+        public Animal(string name, string breed, string color, int age)
+        {
+            this.name = name;
+            this.breed = breed;
+            this.color = color;
+            this.age = age;
+        }
+        public string GetDetails()
+        {
+            return $"{name} {breed} {age}";
+        }
+    }
+}
+    
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Constructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var animal1 = new Animal("Dog", "Doberman", "Black", 8);
+            
+            Console.WriteLine(animal1.GetDetails());
+        }
+    }
+}   
